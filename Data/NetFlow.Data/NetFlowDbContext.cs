6661c1fbade36fd12517282e6a2c0ebfcce38ba2
Mihail Tanev <mihail.v.tanev@gmail.com>
2019-07-02 12:22:02 +0200

    Add project files.

---
+++ Data/NetFlow.Data/NetFlowDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetFlow.Data
+{
+    public class NetFlowDbContext : IdentityDbContext
+    {
+        public NetFlowDbContext(DbContextOptions<NetFlowDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Web/NetFlow.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetFlow.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
